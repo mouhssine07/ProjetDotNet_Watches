58f5a485eb474bc8835f49105c248cef5dba19c5
SETUP GAME <SETUP GAME@DESKTOP-9G1TEIU>
2024-12-19 22:53:50 +0100

    Add project files.

---
+++ ProjetDotNet_Watches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetDotNet_Watches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ProjetDotNet_Watches/Views/Home/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetDotNet_Watches.Views.Home
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
