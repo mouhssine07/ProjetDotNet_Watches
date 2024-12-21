f453557feea8259347900f2b54d113c7c437500f
mouhssine07 <127603398+mouhssine07@users.noreply.github.com>
2024-12-21 12:45:59 +0100

    Update HomeController.cs

--- ProjetDotNet_Watches/Controllers/HomeController.cs
+++ ProjetDotNet_Watches/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using ProjetDotNet_Watches.Models;
 using System.Diagnostics;
 
@@ -14,6 +15,26 @@ namespace ProjetDotNet_Watches.Controllers
         }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            return View();
+        }
+
+        public IActionResult Testimonial()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
